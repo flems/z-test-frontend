1fb6a604a881d541327938798fd86cd4cb248c19
flems <borisov.s3@yandex.ru>
2021-10-13 20:18:24 +0300

    Update build.js

--- frontend/build/build.js
+++ frontend/build/build.js
@@ -7,7 +7,8 @@ const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
-const isWatch = process.env.WATCH == 'true' ? true : false;
+
+const isWatch = Boolean(process.env.WATCH);
 const webpackConfig = process.env.NODE_ENV == 'production' ? require('./webpack.prod.conf') : require('./webpack.dev.conf');
 const strSpinner = process.env.NODE_ENV == 'production' ? 'building for production...' : 'building for development...';
 const spinner = ora(strSpinner)
@@ -25,14 +26,14 @@ rm(path.join(config.prod.assetsRoot, config.prod.assetsSubDirectory), err => {
             chunks: false,
             chunkModules: false
         }) + '\n\n')
-        
+
         if (stats.hasErrors()) {
             console.log(chalk.red('  Build failed with errors.\n'))
             process.exit(1)
         }
-        
+
         console.log(chalk.cyan('  Build complete.\n'))
-        
+
         if (isWatch) {
             console.log(chalk.cyan('  Watching...\n'))
         }
